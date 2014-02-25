4c1e0be23c06fd951fd49d64194bbd6eb64a9467
xiefei <xiefei@gtan.com>
2014-02-25 17:08:24 +0800

    mile1 update for windows

--- mile1.js
+++ mile1.js
@@ -30,25 +30,37 @@ function mkdirs(path) {
 mkdirs(mile1Home);
 var mile1JarFilePath = mile1Home + '\\mile1.jar';
 
-(function(ws){
-    if(!fso.FileExists(mile1JarFilePath)) {
-        ws.stdout.writeLine("Installing Mile1..");
-        var xHttp = new ActiveXObject("Microsoft.XMLHTTP");
-        var bStrm = new ActiveXObject("Adodb.Stream");
-        xHttp.Open('GET', 'http://git.oschina.net/43284683/Mile1/raw/master/downloads/mile1.jar', false);
-        xHttp.Send();
 
-        bStrm.type = 1; // binary
-        bStrm.open();
-        bStrm.write(xHttp.responseBody);
-        bStrm.savetofile(mile1JarFilePath, 2); // overwrite
+(function(ws){
+    function download_mile1_jar() {
+            var xHttp = new ActiveXObject("Microsoft.XMLHTTP");
+            var bStrm = new ActiveXObject("Adodb.Stream");
+            xHttp.Open('GET', 'http://git.oschina.net/43284683/Mile1/raw/master/downloads/mile1.jar', false);
+            xHttp.Send();
+
+            bStrm.type = 1; // binary
+            bStrm.open();
+            bStrm.write(xHttp.responseBody);
+            bStrm.savetofile(mile1JarFilePath, 2); // overwrite
+            bStrm.close();
+            ws.stdout.writeLine("Done.")
+    }
+    if(!fso.FileExists(mile1JarFilePath)) {
+        ws.stdout.write("Installing Mile1..");
+        download_mile1_jar();
     }
     var scriptFilePath = ws.scriptFullName;
     var scriptDirectoryPath = fso.GetParentFolderName(fso.GetFile(scriptFilePath))
-    var cmdline = 'cmd /c java -Dmile1.script.path="' + scriptDirectoryPath + '" -jar "' + mile1JarFilePath + '" ' + ws.arguments.join(' ') + ' 2>&1';
-    ws.echo(cmdline);
-    var javaProcess = oShell.exec(cmdline);
-    do{
-        ws.stdout.writeLine(javaProcess.stdout.readLine())
-    } while (javaProcess.stdout.atEndOfStream)
+    if(!ws.arguments[0] == 'update'){
+        var cmdline = 'cmd /c java -Dmile1.script.path="' + scriptDirectoryPath + '" -jar "' + mile1JarFilePath + '" ' + ws.arguments.join(' ') + ' 2>&1';
+        ws.echo(cmdline);
+        var javaProcess = oShell.exec(cmdline);
+        do{
+            ws.stdout.writeLine(javaProcess.stdout.readLine())
+        } while (javaProcess.stdout.atEndOfStream)
+    } else {
+       fso.DeleteFile(mile1JarFilePath);
+       ws.stdout.write("Updating Mile1...");
+       download_mile1_jar();
+    }
 })(WScript);
