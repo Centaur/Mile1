9eddc1439bbb600cf20d31c3448fa6c368c50b48
xiefei <xiefei@gtan.com>
2014-11-17 18:27:28 +0800

    correct type in use/ask

--- src/com/gtan/mile1/wizard.clj
+++ src/com/gtan/mile1/wizard.clj
@@ -1,19 +1,21 @@
 (ns com.gtan.mile1.wizard
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [com.gtan.mile1.sbt-version :as sbt-version]))
 
-(defn ask
+(defn ^String ask
   "show a wizard , retrieve a string from user"
   [{:keys [prompt options format default] :as wizard-config}]
-  (try
-    (doseq [option (map-indexed #(str "[" %1 "]" %2) options)]
-      (println option))
-    (print (str prompt (if default (str "(默认 " (.indexOf options default) ")")) ":"))
-    (flush)
-    (let [input (read-line)]
-      (if (empty? input)
-        default
-        (case format
-          :indexed (nth options (Integer/parseInt input))
-          :text input)))
-    (catch Exception e (ask wizard-config))))
+  (let [version-strs (map sbt-version/version->str options)]
+    (try
+      (doseq [version (map-indexed #(str "[" %1 "]" %2) version-strs)]
+        (println version))
+      (print (str prompt (if default (str "(默认 " (.indexOf version-strs default) ")")) ":"))
+      (flush)
+      (let [input (read-line)]
+        (if (empty? input)
+          default
+          (case format
+            :indexed (sbt-version/version->str (nth options (Integer/parseInt input)))
+            :text input)))
+      (catch Exception e (ask wizard-config)))))
 
